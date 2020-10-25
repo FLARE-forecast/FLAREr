fd11d5418376960702c60637228e16744b3c9534
rqthomas <rqthomas@vt.edu>
2020-10-25 15:07:55 -0400

    adding spatial downscaling

--- R/generate_glm_met_files.R
+++ R/generate_glm_met_files.R
@@ -7,6 +7,9 @@
 #' @param end_datetime
 #' @param forecast_start_datetime
 #' @param local_tzone
+#' @param use_forecasted_met
+#' @param spatial_downscale
+#' @param spatial_downscale_coeff
 #'
 #' @return
 #' @export
@@ -18,7 +21,10 @@ generate_glm_met_files <- function(obs_met_file = NULL,
                                    local_tzone,
                                    start_datetime_local,
                                    end_datetime_local,
-                                   forecast_start_datetime_local){
+                                   forecast_start_datetime_local,
+                                   use_forecasted_met,
+                                   spatial_downscale = FALSE,
+                                   spatial_downscale_coeff = NULL){
 
   if(is.null(obs_met_file) & is.null(forecast_dir)){
     stop("missing files to convert")
@@ -29,7 +35,11 @@ generate_glm_met_files <- function(obs_met_file = NULL,
   forecast_start_datetime_UTC <- lubridate::with_tz(forecast_start_datetime_local, tzone = "UTC")
 
   full_time_UTC <- seq(start_datetime_UTC, end_datetime_UTC, by = "1 hour")
-  full_time_UTC_hist <- seq(start_datetime_UTC, forecast_start_datetime_UTC, by = "1 hour")
+  if(use_forecasted_met){
+    full_time_UTC_hist <- seq(start_datetime_UTC, forecast_start_datetime_UTC, by = "1 hour")
+  }else{
+    full_time_UTC_hist <- seq(start_datetime_UTC, end_datetime_UTC, by = "1 hour")
+  }
   cf_met_vars <- c("air_temperature",
                    "surface_downwelling_shortwave_flux_in_air",
                    "surface_downwelling_longwave_flux_in_air",
@@ -90,49 +100,57 @@ generate_glm_met_files <- function(obs_met_file = NULL,
 
   for(j in 1:nfiles){
 
-
-
-    if(!is.null(forecast_dir)){
+    if(!is.null(forecast_dir) & use_forecasted_met){
 
 
       ens <- dplyr::last(unlist(stringr::str_split(basename(forecast_files[j]),"_")))
-
       ens <- stringr::str_sub(ens,1,5)
-
       noaa_met_nc <- ncdf4::nc_open(forecast_files[j])
-
       noaa_met_time <- ncdf4::ncvar_get(noaa_met_nc, "time")
-
       origin <- stringr::str_sub(ncdf4::ncatt_get(noaa_met_nc, "time")$units, 13, 28)
-
       origin <- lubridate::ymd_hm(origin)
-
       noaa_met_time <- origin + lubridate::hours(noaa_met_time)
-
       noaa_met <- tibble::tibble(time = noaa_met_time)
 
       for(i in 1:length(cf_met_vars)){
-
         noaa_met <- cbind(noaa_met, ncdf4::ncvar_get(noaa_met_nc, cf_met_vars[i]))
       }
 
       ncdf4::nc_close(noaa_met_nc)
-
-
       names(noaa_met) <- c("time", glm_met_vars)
+
+      if(spatial_downscale){
+
+        noaa_met_daily <- noaa_met %>%
+          mutate(date = lubridate::as_date(time)) %>%
+          select(-time) %>%
+          group_by(date) %>%
+          summarise_all(mean, na.rm = TRUE) %>%
+          mutate(AirTemp_debias = spatial_downscale_coeff$AirTemp[1] + AirTemp * spatial_downscale_coeff$AirTemp[2],
+                 AirTemp_orig = AirTemp,
+                 ShortWave_debias = spatial_downscale_coeff$ShortWave[1] + ShortWave * spatial_downscale_coeff$ShortWave[2],
+                 ShortWave_orig = ShortWave,
+                 LongWave_debias = spatial_downscale_coeff$LongWave[1] + LongWave * spatial_downscale_coeff$LongWave[2],
+                 LongWave_orig = LongWave) %>%
+          select(date, AirTemp_debias, AirTemp_orig, ShortWave_debias, ShortWave_orig, LongWave_debias, LongWave_orig)
+
+        noaa_met <- noaa_met %>%
+          mutate(date = lubridate::as_date(time)) %>%
+          left_join(noaa_met_daily, by = "date") %>%
+          mutate(AirTemp = (AirTemp/AirTemp_orig) * AirTemp_debias,
+                 ShortWave = (ShortWave/ShortWave_orig) * ShortWave_debias,
+                 LongWave = (LongWave/LongWave_orig) * LongWave_debias) %>%
+          select(time, AirTemp, ShortWave, LongWave, RelHum, WindSpeed, Rain)
+      }
 
       noaa_met <- noaa_met %>%
         dplyr::filter(time %in% full_time_UTC)
 
-
       combined_met <- rbind(met, noaa_met)
-
-
       current_filename <- paste0('met_',ens,'.csv')
     }else{
 
       combined_met <- met
-
       current_filename <- paste0('met.csv')
     }
 
