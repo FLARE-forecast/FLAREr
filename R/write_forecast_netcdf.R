d56aed9f03b292a3628be6ecde06c47af0082ddb
tadhg-moore <tadhgm@vt.edu>
2021-03-22 11:14:24 -0400

    moodel -> model

--- R/write_forecast_netcdf.R
+++ R/write_forecast_netcdf.R
@@ -77,7 +77,7 @@ write_forecast_netcdf <- function(enkf_output,
   def_list[[6]] <- ncdf4::ncvar_def("lake_depth","meter",list(timedim,ensdim),missval = -99,longname = 'Depth of lake',prec="single")
   def_list[[7]] <- ncdf4::ncvar_def("avg_surf_temp","degC",list(timedim, ensdim),missval = -99,longname ='Running Average of Surface Temperature',prec="single")
   def_list[[8]] <- ncdf4::ncvar_def("mixing_vars","dimensionless",list(mixing_vars_dim, timedim, ensdim),fillvalue,longname = "variables required to restart mixing",prec="single")
-  def_list[[9]] <- ncdf4::ncvar_def("model_internal_depths","meter",list(timedim, internal_moodel_depths_dim, ensdim),fillvalue,longname = "depths simulated by glm that are required to restart ",prec="single")
+  def_list[[9]] <- ncdf4::ncvar_def("model_internal_depths","meter",list(timedim, internal_model_depths_dim, ensdim),fillvalue,longname = "depths simulated by glm that are required to restart ",prec="single")
   def_list[[10]] <- ncdf4::ncvar_def("salt","g_kg",list(timedim, depthdim, ensdim),fillvalue,longname = "salt",prec="single")
 
 
