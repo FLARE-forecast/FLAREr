a0913cabf5e296a294a7e8d8f8473cb41a65a369
tadhg-moore <tadhgm@vt.edu>
2021-04-09 14:26:29 -0400

    Added check for if there is only one layer in the nc file

--- R/define_model_glm_aed.R
+++ R/define_model_glm_aed.R
@@ -292,7 +292,14 @@ run_model <- function(i,
                    finally = NULL)
 
     if(!is.null(nc)){
-      success <- TRUE
+      # Catch for if the output has more than one layer
+      message("'output.nc' file generated but less than one layer in the file")
+      tallest_layer <- ncdf4::ncvar_get(nc, "NS")
+      if(tallest_layer > 1) {
+        success <- TRUE
+      } else {
+        success <- FALSE
+      }
     }else{
       success <- FALSE
     }
