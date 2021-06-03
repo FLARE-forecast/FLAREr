03db5f54fcac2a45f6ef03eb350f959ac8a32cdd
rqthomas <rqthomas@vt.edu>
2021-06-03 12:50:44 -0400

    forecast_project_id set as sim_name

--- R/write_forecast_netcdf.R
+++ R/write_forecast_netcdf.R
@@ -186,7 +186,7 @@ write_forecast_netcdf <- function(enkf_output,
   #Global file metadata
   ncdf4::ncatt_put(ncout,0,"title",enkf_output$config$metadata$forecast_title, prec =  "text")
   ncdf4::ncatt_put(ncout,0,"forecast_iteration_id",enkf_output$forecast_iteration_id, prec =  "text")
-  ncdf4::ncatt_put(ncout,0,"forecast_project_id",enkf_output$config$metadata$forecast_project_id, prec =  "text")
+  ncdf4::ncatt_put(ncout,0,"forecast_project_id",enkf_output$forecast_project_id, prec =  "text")
   ncdf4::ncatt_put(ncout,0,"forecast_model_id",enkf_output$config$metadata$model_description$forecast_model_id, prec =  "text")
   ncdf4::ncatt_put(ncout,0,"time_zone_of_simulation","UTC", prec =  "text")
 
