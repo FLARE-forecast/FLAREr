eb005155941620e52599274b810b7910909f6c7a
Quinn Thomas <rqthomas@vt.edu>
2023-01-27 10:16:50 -0500

    Update generate_forecast_score.R

--- R/generate_forecast_score.R
+++ R/generate_forecast_score.R
@@ -21,19 +21,21 @@ generate_forecast_score <- function(targets_file,
   fn <- tools::file_path_sans_ext(tools::file_path_sans_ext(basename(forecast_file)))
 
   file_name <- file.path(output_directory,paste0(fn, ".parquet"))
-
+  
   forecast <- forecast_file %>%
     read4cast::read_forecast() %>%
-    select(-pub_time) %>%
-    filter(variable_type == "state") %>%
-    dplyr::mutate(filename = forecast_file,
-                  site_id = paste0(site_id,"-",depth)) %>%
-    score4cast::standardize_forecast() %>%
+    dplyr::select(-pub_time) %>%
+    dplyr::filter(variable_type %in% variable_types) %>%
+    dplyr::mutate(site_id = paste0(site_id,"-",depth)) %>%
+    score4cast::standardize_forecast(reference_datetime_format = "%Y-%m-%d %H:%M:%S") %>%
+    dplyr::mutate(family = as.character(family)) |>
     score4cast::crps_logs_score(target) %>%
-    mutate(horizon = datetime-lubridate::as_datetime(reference_datetime)) %>%
-    mutate(horizon = as.numeric(lubridate::as.duration(horizon),
+    dplyr::mutate(horizon = datetime-lubridate::as_datetime(reference_datetime)) %>%
+    dplyr::mutate(horizon = as.numeric(lubridate::as.duration(horizon),
                                 units = "seconds"),
            horizon = horizon / 86400) %>%
+    dplyr::mutate(depth = as.numeric(str_split_fixed(site_id, "-", 2)[,2]),
+           site_id = str_split_fixed(site_id, "-", 2)[,1]) %>%
     arrow::write_parquet(file_name)
 
   invisible(file_name)
