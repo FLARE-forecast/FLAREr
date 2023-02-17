a6a2a398151691f39fb5756f6e327675532ee725
Quinn Thomas <rqthomas@vt.edu>
2023-02-17 09:07:23 -0500

    changed to curl_timeout

--- R/workflow_functions.R
+++ R/workflow_functions.R
@@ -77,7 +77,7 @@ get_git_repo <- function(lake_directory, directory, git_repo){
 #' @return
 #' @export
 #'
-get_edi_file <- function(edi_https, file, lake_directory, run_time = 60){
+get_edi_file <- function(edi_https, file, lake_directory, curl_timeout = 60){
 
   if(!file.exists(file.path(lake_directory, "data_raw", file))){
     if(!dir.exists(dirname(file.path(lake_directory, "data_raw", file)))){
@@ -86,7 +86,7 @@ get_edi_file <- function(edi_https, file, lake_directory, run_time = 60){
     download.file(edi_https,
                   destfile = file.path(lake_directory, "data_raw", file),
                   method="curl",
-                 extra = options(timeout = run_time))
+                 extra = options(timeout = curl_timeout))
   }
 }
 
