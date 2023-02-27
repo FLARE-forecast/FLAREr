07621074e4125e9ac77906343fcf8d44d3cf40c8
Austin Delany <addelany@gmail.com>
2023-02-27 11:55:16 -0600

    add namespace for content variable in get_edi_file

--- R/workflow_functions.R
+++ R/workflow_functions.R
@@ -84,7 +84,7 @@ get_edi_file <- function(edi_https, file, lake_directory){ #, curl_timeout = 60)
       dir.create(dirname(file.path(lake_directory, "data_raw", file)))
     }
     url_download <- httr::RETRY("GET",edi_https,timeout(120),times = 3, quiet = FALSE)
-    test_bin <- content(url_download,'raw')
+    test_bin <- httr::content(url_download,'raw')
     writeBin(test_bin, file.path(lake_directory, "data_raw", file))
   }
 }
