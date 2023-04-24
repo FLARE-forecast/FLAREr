2ac0fcd35ab6c32efa01d79801adf2334da4c9b4
rqthomas <rqthomas@vt.edu>
2023-04-24 10:36:26 -0400

    localization clean up

--- R/localization.R
+++ R/localization.R
@@ -4,12 +4,13 @@
 #' @param nstates number of states
 #' @param modeled_depths vector of depths that are modeled
 #' @param localization_distance distance scalar for the strenght of the locational
+#' @param num_single_states number of states that to do not have values for each depth (e.g. secchi)
 #'
 #' @return matrix with localization applied
 #' @noRd
 localization <- function(mat,nstates,modeled_depths, localization_distance, num_single_states = 0){
 
-  distance_matrix <- matrix(NA, nrow = nstates*modeled_depths, ncol = nstates*modeled_depths)
+  distance_matrix <- matrix(NA, nrow = nstates* length(modeled_depths), ncol = nstates*length(modeled_depths))
 
   for(i in 1:dim(distance_matrix)[1]){
     index <- 1
@@ -25,11 +26,18 @@ localization <- function(mat,nstates,modeled_depths, localization_distance, num_
   distance_differ_matrix <- distance_matrix - diag(distance_matrix)
 
   for(i in 1:dim(distance_matrix)[1]){
-  distance_differ_matrix[i, ] <- exp(-(distance_differ_matrix[i, ]/localization_distance)^2)
+    distance_differ_matrix[i, ] <- exp(-(distance_differ_matrix[i, ]/localization_distance)^2)
   }
-  
-  mat_new <- mat * distance_differ_matrix
+  if(num_single_states > 0){
+    for(i in 1:num_single_states){
+      new_col <- rep(1, nrow(distance_differ_matrix))
+      distance_differ_matrix <- cbind(distance_differ_matrix, new_col)
+      new_row <- rep(1, ncol(distance_differ_matrix))
+      distance_differ_matrix <- rbind(distance_differ_matrix, new_row)
+    }
+  }
 
+  mat_new <- mat * distance_differ_matrix
 
   return(mat_new)
 
