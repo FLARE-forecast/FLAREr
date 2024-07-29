fa3e3201e94e9049794e3a77e41081d78834324d
addelany <addelany@gmail.com>
2024-07-29 11:42:07 -0500

    update plotting function

--- R/plotting_general_3.R
+++ R/plotting_general_3.R
@@ -24,7 +24,6 @@ plotting_general_3 <- function(forecast_object,
   }else{
     plot_height <- 8
   }
-
   pdf(pdf_file_name,width = 11, height = plot_height)
 
   state_depth_variables <- forecast_object |>
@@ -33,23 +32,29 @@ plotting_general_3 <- function(forecast_object,
     distinct(variable) |>
     pull(variable)
 
-  ## BUILD DEPTH-SPECIFIC STATE VARIABLE PLOTS
+  target_depths <- targets_object |>
+    filter(variable == 'Temp_C_mean',
+           !is.na(depth)) |>
+    distinct(depth) |>
+    pull(depth)
+
+  ## build state variable plots
   for (var in state_depth_variables){
     message(var)
 
-    var_target_depths <- targets_object |>
-      filter(variable == var,
-             !is.na(depth)) |>
-      distinct(depth) |>
-      pull(depth)
+    var_targets <- targets_object |>
+      filter(variable == var, !is.na(depth))
 
-    if (length(var_target_depths) == 0){
+    if (nrow(var_targets) == 0){
       state_plot <- forecast_object |>
-        filter(variable == var) |>
+        filter(variable == var,
+               depth %in% target_depths) |>
         ggplot(aes(x = datetime)) +
         geom_line(aes(y = prediction, group = parameter)) +
+        #geom_point(aes(y = observation), color = "red") +
         geom_vline(aes(xintercept = reference_datetime)) +
         theme_bw() +
+        facet_wrap(~depth, scales = "free") +
         labs(y = "forecast") +
         ggtitle(var) +
         theme(plot.title = element_text(hjust = 0.5))
@@ -57,7 +62,7 @@ plotting_general_3 <- function(forecast_object,
     }else{
       state_plot <- forecast_object |>
         filter(variable == var,
-               depth %in% var_target_depths) |>
+               depth %in% target_depths) |>
         left_join(targets_object) |>
         ggplot(aes(x = datetime)) +
         geom_line(aes(y = prediction, group = parameter)) +
@@ -69,12 +74,12 @@ plotting_general_3 <- function(forecast_object,
         ggtitle(var) +
         theme(plot.title = element_text(hjust = 0.5))
 
-        }
+    }
 
     plot(state_plot)
   }
 
-  ## BUILD NON-DEPTH-SPECIFIC STATE VARIABLE PLOTS
+  ## build non-depth specific variable plots
   state_non_depth_variables <- forecast_object |>
     filter(variable_type == 'state',
            variable %in% c('secchi', 'depth', 'ice_thickness')) |>
@@ -91,33 +96,36 @@ plotting_general_3 <- function(forecast_object,
     geom_vline(aes(xintercept = reference_datetime)) +
     theme_bw() +
     facet_wrap(~variable, scales = "free") +
-    labs(y = "forecast")
+    labs(y = "forecast") #+
+  # ggtitle(state_depth_variables[i]) +
+  # theme(plot.title = element_text(hjust = 0.5))
 
   plot(state_non_depth_plot)
 
 
-  ## CREATE PARAMETER PLOTS
+  ## create parameter plot
   parameter_variables <- forecast_object |>
     filter(variable_type == 'parameter') |>
     distinct(variable) |>
     pull(variable)
 
-  parameter_plot <- forecast_object |>
-    filter(variable == parameter_variables) |>
-    #left_join(targets_object) |>
-    #dplyr::filter(variable %in% c("temperature","secchi", "chla", "lw_factor") & (depth == 1.5 | is.na(depth))) |>
-    ggplot(aes(x = datetime)) +
-    geom_line(aes(y = prediction, group = parameter)) +
-    #geom_point(aes(y = observation), color = "red") +
-    geom_vline(aes(xintercept = reference_datetime)) +
-    theme_bw() +
-    facet_wrap(~variable, scales = "free") +
-    labs(y = "forecast")
+  if (length(parameter_variables) != 0){
+    parameter_plot <- forecast_object |>
+      filter(variable == parameter_variables) |>
+      #left_join(targets_object) |>
+      #dplyr::filter(variable %in% c("temperature","secchi", "chla", "lw_factor") & (depth == 1.5 | is.na(depth))) |>
+      ggplot(aes(x = datetime)) +
+      geom_line(aes(y = prediction, group = parameter)) +
+      #geom_point(aes(y = observation), color = "red") +
+      geom_vline(aes(xintercept = reference_datetime)) +
+      theme_bw() +
+      facet_wrap(~variable, scales = "free") +
+      labs(y = "forecast")
 
-  plot(parameter_plot)
+    plot(parameter_plot)
+  }
 
-
-  ## CREATE DIAGNOSTIC PLOTS
+  ## create diagnostic plots
   diagnostic_variables <- forecast_object |>
     filter(variable_type == 'diagnostic') |>
     distinct(variable) |>
@@ -135,8 +143,11 @@ plotting_general_3 <- function(forecast_object,
     diagnostic_plot <- forecast_object |>
       filter(variable == var,
              depth %in% diagnostic_target_depths) |>
+      #left_join(targets_object) |>
+      #dplyr::filter(variable %in% c("temperature","secchi", "chla", "lw_factor") & (depth == 1.5 | is.na(depth))) |>
       ggplot(aes(x = datetime)) +
       geom_line(aes(y = prediction, group = parameter)) +
+      #geom_point(aes(y = observation), color = "red") +
       geom_vline(aes(xintercept = reference_datetime)) +
       theme_bw() +
       facet_wrap(~depth, scales = "free") +
