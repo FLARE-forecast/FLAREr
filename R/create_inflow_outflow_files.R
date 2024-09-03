6903583d45c208a0c91c1e0c40719d24545df0b1
Freya E Olsson <freyao@tc100.cm.cluster>
2024-09-03 10:04:15 -0400

    new use_s3 config for flow

--- R/create_inflow_outflow_files.R
+++ R/create_inflow_outflow_files.R
@@ -7,10 +7,7 @@
 #' @keywords internal
 #'
 create_inflow_outflow_files  <- function(config, config_set_name, lake_directory) {
-  # set use_s3 to T if missing
-  if(is.null(config$run_config$use_s3)){
-    config$run_config$use_s3 <- TRUE
-  }
+
 
   # The variables differ between inflow and outflow
   #variables_in <- unique(c('time',
@@ -87,7 +84,7 @@ create_inflow_outflow_files  <- function(config, config_set_name, lake_directory
     # forecast_start_datetime = config$run_config$forecast_start_datetime
     # forecast_horizon = config$run_config$forecast_horizon
     # site_id = config$location$site_id
-    # use_s3 = config$run_config$use_s3
+    # use_s3 = config$flows$use_flows_s3
     # bucket = config$s3$inflow_drivers$bucket
     # endpoint = config$s3$inflow_drivers$endpoint
     # local_directory = file.path(lake_directory, config$flows$local_inflow_directory)
@@ -105,7 +102,7 @@ create_inflow_outflow_files  <- function(config, config_set_name, lake_directory
                                              forecast_start_datetime = config$run_config$forecast_start_datetime ,
                                              forecast_horizon = config$run_config$forecast_horizon ,
                                              site_id = config$location$site_id ,
-                                             use_s3 = config$run_config$use_s3 ,
+                                             use_s3 = config$flows$use_flows_s3 ,
                                              bucket = list(config$s3$inflow_drivers$bucket , config$s3$outflow_drivers$bucket),
                                              endpoint = list(config$s3$inflow_drivers$endpoint , config$s3$outflow_drivers$endpoint) ,
                                              local_directory = list(file.path(lake_directory, config$flows$local_inflow_directory),
@@ -154,7 +151,7 @@ create_inflow_outflow_files  <- function(config, config_set_name, lake_directory
                                              forecast_start_datetime = config$run_config$forecast_start_datetime ,
                                              forecast_horizon = config$run_config$forecast_horizon ,
                                              site_id = config$location$site_id ,
-                                             use_s3 = config$run_config$use_s3 ,
+                                             use_s3 = config$flows$use_flows_s3 ,
                                              bucket = list(config$s3$inflow_drivers$bucket , config$s3$outflow_drivers$bucket),
                                              endpoint = config$s3$inflow_drivers$endpoint ,
                                              local_directory = list(file.path(lake_directory, config$flows$local_inflow_directory),
@@ -203,7 +200,7 @@ create_inflow_outflow_files  <- function(config, config_set_name, lake_directory
                                              forecast_start_datetime = config$run_config$forecast_start_datetime ,
                                              forecast_horizon = config$run_config$forecast_horizon ,
                                              site_id = config$location$site_id ,
-                                             use_s3 = config$run_config$use_s3 ,
+                                             use_s3 = config$flows$use_flows_s3 ,
                                              bucket = list(config$s3$inflow_drivers$bucket , config$s3$outflow_drivers$bucket),
                                              endpoint = config$s3$inflow_drivers$endpoint ,
                                              local_directory = list(file.path(lake_directory, config$flows$local_inflow_directory),
