d7db26da8f0e1c13e419d78d1235b7478f6cc134
rqthomas <rqthomas@vt.edu>
2026-05-13 21:09:11 -0400

    Speeding up the forecast write step

--- R/write_forecast.R
+++ R/write_forecast.R
@@ -67,29 +67,26 @@ write_forecast <- function(da_forecast_output,
   obs_config <- obs_config |>
     dplyr::filter(multi_depth == 1)
 
-  indexes <- expand.grid(time = 1:dim(x)[1], states = 1:dim(x)[2], depths = 1:dim(x)[3])
-  ensembles <- 1:dim(x)[4]
+  pieces <- list()
 
-  if(config$model_settings$ncore == 1){
-    future::plan("future::sequential", workers = config$model_settings$ncore)
-  }else{
-    future::plan("future::multisession", workers = config$model_settings$ncore)
-  }
+  # --- states: x[time, states, depths, ens] ---
+  n_time   <- dim(x)[1]
+  n_states <- dim(x)[2]
+  n_depths <- dim(x)[3]
+  n_ens    <- dim(x)[4]
 
-  output_list <- furrr::future_map_dfr(1:nrow(indexes), function(i, indexes){
-    var1 <- indexes$time[i]
-    var2 <- indexes$states[i]
-    var3 <- indexes$depths[i]
-    tibble::tibble(predicted = x[var1, var2, var3, ],
-                   time  = full_time[var1],
-                   depth = config$model_settings$modeled_depths[var3],
-                   variable = states_config$state_names[var2],
-                   forecast = forecast_flag[var1],
-                   ensemble = ensembles,
-                   variable_type = "state",
-                   log_weight = log_particle_weights[var1, ])
-  },
-  indexes = indexes
+  x_mat <- matrix(x, nrow = n_time * n_states * n_depths, ncol = n_ens)
+  idx   <- expand.grid(time = seq_len(n_time), states = seq_len(n_states), depths = seq_len(n_depths))
+
+  pieces$states <- tibble::tibble(
+    predicted     = c(t(x_mat)),
+    time          = rep(full_time[idx$time],                              each = n_ens),
+    depth         = rep(config$model_settings$modeled_depths[idx$depths], each = n_ens),
+    variable      = rep(states_config$state_names[idx$states],            each = n_ens),
+    forecast      = rep(forecast_flag[idx$time],                          each = n_ens),
+    ensemble      = rep(seq_len(n_ens), nrow(idx)),
+    variable_type = "state",
+    log_weight    = c(t(log_particle_weights[idx$time, ]))
   )
 
   tmp_index <- 0
@@ -110,134 +107,153 @@ write_forecast <- function(da_forecast_output,
         }
       }
 
-      indexes <- expand.grid(time = 1:dim(temp_var)[1], depth = 1:dim(temp_var)[2])
+      # --- derived obs: temp_var[time, depths, ens] ---
+      n_time_tv   <- dim(temp_var)[1]
+      n_depths_tv <- dim(temp_var)[2]
+      n_ens_tv    <- dim(temp_var)[3]
 
-      output_list_tmp <- furrr::future_map_dfr(1:nrow(indexes), function(i, indexes){
-        var1 <- indexes$time[i]
-        var3 <- indexes$depth[i]
-        tibble::tibble(predicted = temp_var[var1, var3, ],
-                       time  = full_time[var1],
-                       depth = config$model_settings$modeled_depths[var3],
-                       variable = obs_config$target_variable[s],
-                       forecast = forecast_flag[var1],
-                       ensemble = ensembles,
-                       variable_type = "state",
-                       log_weight = log_particle_weights[var1, ])
-      },
-      indexes = indexes
+      tv_mat <- matrix(temp_var, nrow = n_time_tv * n_depths_tv, ncol = n_ens_tv)
+      idx_tv <- expand.grid(time = seq_len(n_time_tv), depth = seq_len(n_depths_tv))
+
+      pieces[[paste0("derived_obs_", tmp_index)]] <- tibble::tibble(
+        predicted     = c(t(tv_mat)),
+        time          = rep(full_time[idx_tv$time],                               each = n_ens_tv),
+        depth         = rep(config$model_settings$modeled_depths[idx_tv$depth],   each = n_ens_tv),
+        variable      = obs_config$target_variable[s],
+        forecast      = rep(forecast_flag[idx_tv$time],                           each = n_ens_tv),
+        ensemble      = rep(seq_len(n_ens_tv), nrow(idx_tv)),
+        variable_type = "state",
+        log_weight    = c(t(log_particle_weights[idx_tv$time, ]))
       )
-      output_list <- bind_rows(output_list, output_list_tmp)
     }
   }
 
 
   if(length(config$output_settings$diagnostics_names) > 0){
 
-    indexes <- expand.grid(diag = 1:dim(diagnostics)[1], time = 1:dim(diagnostics)[2], depth = 1:dim(diagnostics)[3])
+    # --- diagnostics: diagnostics[diag, time, depth, ens] ---
+    n_diag_d  <- dim(diagnostics)[1]
+    n_time_d  <- dim(diagnostics)[2]
+    n_depth_d <- dim(diagnostics)[3]
+    n_ens_d   <- dim(diagnostics)[4]
 
-    tmp <- furrr::future_map_dfr(1:nrow(indexes), function(i, indexes){
-      var1 <- indexes$diag[i]
-      var2 <- indexes$time[i]
-      var3 <- indexes$depth[i]
-      tibble::tibble(predicted = diagnostics[var1, var2, var3, ],
-                     time  = full_time[var2],
-                     depth = config$model_settings$modeled_depths[var3],
-                     variable = config$output_settings$diagnostics_names[var1],
-                     forecast = forecast_flag[var2],
-                     ensemble = ensembles,
-                     variable_type = "diagnostic",
-                     log_weight = log_particle_weights[var2, ])
-    },
-    indexes = indexes
+    d_mat <- matrix(diagnostics, nrow = n_diag_d * n_time_d * n_depth_d, ncol = n_ens_d)
+    idx_d <- expand.grid(diag = seq_len(n_diag_d), time = seq_len(n_time_d), depth = seq_len(n_depth_d))
+
+    pieces$diagnostics <- tibble::tibble(
+      predicted     = c(t(d_mat)),
+      time          = rep(full_time[idx_d$time],                                 each = n_ens_d),
+      depth         = rep(config$model_settings$modeled_depths[idx_d$depth],     each = n_ens_d),
+      variable      = rep(config$output_settings$diagnostics_names[idx_d$diag],  each = n_ens_d),
+      forecast      = rep(forecast_flag[idx_d$time],                              each = n_ens_d),
+      ensemble      = rep(seq_len(n_ens_d), nrow(idx_d)),
+      variable_type = "diagnostic",
+      log_weight    = c(t(log_particle_weights[idx_d$time, ]))
     )
-    output_list <- dplyr::bind_rows(output_list, tmp)
   }
 
 
 
   if(length(config$output_settings$diagnostics_daily$names) > 0){
 
-    indexes <- expand.grid(diag = 1:dim(diagnostics_daily)[1], time = 1:dim(diagnostics_daily)[2])
+    # --- diagnostics_daily: diagnostics_daily[diag, time, ens] ---
+    n_diag_dd <- dim(diagnostics_daily)[1]
+    n_time_dd <- dim(diagnostics_daily)[2]
+    n_ens_dd  <- dim(diagnostics_daily)[3]
 
-    tmp <- furrr::future_map_dfr(1:nrow(indexes), function(i, indexes){
-      var1 <- indexes$diag[i]
-      var2 <- indexes$time[i]
-      tibble::tibble(predicted = diagnostics_daily[var1, var2, ],
-                     time  = full_time[var2],
-                     depth = NA,
-                     variable = config$output_settings$diagnostics_daily$save_names[var1],
-                     forecast = forecast_flag[var2],
-                     ensemble = ensembles,
-                     variable_type = "diagnostic",
-                     log_weight = log_particle_weights[var2, ])
-    },
-    indexes = indexes
+    dd_mat <- matrix(diagnostics_daily, nrow = n_diag_dd * n_time_dd, ncol = n_ens_dd)
+    idx_dd <- expand.grid(diag = seq_len(n_diag_dd), time = seq_len(n_time_dd))
+
+    pieces$diagnostics_daily <- tibble::tibble(
+      predicted     = c(t(dd_mat)),
+      time          = rep(full_time[idx_dd$time],                                             each = n_ens_dd),
+      depth         = NA,
+      variable      = rep(config$output_settings$diagnostics_daily$save_names[idx_dd$diag],  each = n_ens_dd),
+      forecast      = rep(forecast_flag[idx_dd$time],                                         each = n_ens_dd),
+      ensemble      = rep(seq_len(n_ens_dd), nrow(idx_dd)),
+      variable_type = "diagnostic",
+      log_weight    = c(t(log_particle_weights[idx_dd$time, ]))
     )
-    output_list <- dplyr::bind_rows(output_list, tmp)
   }
 
   if(!is.null(pars)){
-    indexes <- expand.grid(time = 1:dim(pars)[1], par = 1:dim(pars)[2])
 
-    tmp <- furrr::future_map_dfr(1:nrow(indexes), function(i, indexes){
-      var1 <- indexes$time[i]
-      var2 <- indexes$par[i]
-      tibble::tibble(predicted = pars[var1, var2, ],
-                     time  = full_time[var1],
-                     depth = NA,
-                     variable = pars_config$par_names_save[var2],
-                     forecast = forecast_flag[var1],
-                     ensemble = ensembles,
-                     variable_type = "parameter",
-                     log_weight = log_particle_weights[var1, ])
-    },
-    indexes = indexes
+    # --- parameters: pars[time, par, ens] ---
+    n_time_p <- dim(pars)[1]
+    n_par    <- dim(pars)[2]
+    n_ens_p  <- dim(pars)[3]
+
+    p_mat <- matrix(pars, nrow = n_time_p * n_par, ncol = n_ens_p)
+    idx_p <- expand.grid(time = seq_len(n_time_p), par = seq_len(n_par))
+
+    pieces$pars <- tibble::tibble(
+      predicted     = c(t(p_mat)),
+      time          = rep(full_time[idx_p$time],                    each = n_ens_p),
+      depth         = NA,
+      variable      = rep(pars_config$par_names_save[idx_p$par],    each = n_ens_p),
+      forecast      = rep(forecast_flag[idx_p$time],                 each = n_ens_p),
+      ensemble      = rep(seq_len(n_ens_p), nrow(idx_p)),
+      variable_type = "parameter",
+      log_weight    = c(t(log_particle_weights[idx_p$time, ]))
     )
-    output_list <- dplyr::bind_rows(output_list, tmp)
   }
 
   if(!is.null(da_forecast_output$restart_list)){
     lake_depth <- da_forecast_output$restart_list$lake_depth
   }
 
-  output_list3 <- furrr::future_map_dfr(1:dim(lake_depth)[1], function(i){
-    tibble::tibble(predicted = lake_depth[i, ],
-                   time  = full_time[i],
-                   variable = "depth",
-                   depth = NA,
-                   forecast = forecast_flag[i],
-                   ensemble = 1:dim(lake_depth)[2],
-                   variable_type = "state",
-                   log_weight = log_particle_weights[i, ])
-  })
-  output_list <- dplyr::bind_rows(output_list, output_list3)
+  # --- lake depth: lake_depth[time, ens] ---
+  n_time_ld <- dim(lake_depth)[1]
+  n_ens_ld  <- dim(lake_depth)[2]
+
+  pieces$lake_depth <- tibble::tibble(
+    predicted     = c(t(lake_depth)),
+    time          = rep(full_time[seq_len(n_time_ld)],      each = n_ens_ld),
+    variable      = "depth",
+    depth         = NA,
+    forecast      = rep(forecast_flag[seq_len(n_time_ld)],  each = n_ens_ld),
+    ensemble      = rep(seq_len(n_ens_ld), n_time_ld),
+    variable_type = "state",
+    log_weight    = c(t(log_particle_weights[seq_len(n_time_ld), ]))
+  )
 
   if(length(config$output_settings$diagnostics_names) > 0){
-    tmp <- furrr::future_map_dfr(1:dim(diagnostics)[2], function(i){
-      tibble::tibble(predicted = 1.7 / diagnostics[1, i, which.min(abs(config$model_settings$modeled_depths-1.0)), ],
-                     time = full_time[i],
-                     variable = "secchi",
-                     depth = NA,
-                     forecast = forecast_flag[i],
-                     ensemble = 1:dim(diagnostics)[4],
-                     variable_type = "state",
-                     log_weight = log_particle_weights[i, ])
-    })
-    output_list <- dplyr::bind_rows(output_list, tmp)
+
+    # --- secchi: derived from diagnostics[1, time, depth_idx, ens] ---
+    depth_idx_secchi <- which.min(abs(config$model_settings$modeled_depths - 1.0))
+    n_time_s <- dim(diagnostics)[2]
+    n_ens_s  <- dim(diagnostics)[4]
+    secchi_mat <- 1.7 / diagnostics[1, , depth_idx_secchi, ]  # [time, ens]
+
+    pieces$secchi <- tibble::tibble(
+      predicted     = c(t(secchi_mat)),
+      time          = rep(full_time[seq_len(n_time_s)],      each = n_ens_s),
+      variable      = "secchi",
+      depth         = NA,
+      forecast      = rep(forecast_flag[seq_len(n_time_s)],  each = n_ens_s),
+      ensemble      = rep(seq_len(n_ens_s), n_time_s),
+      variable_type = "state",
+      log_weight    = c(t(log_particle_weights[seq_len(n_time_s), ]))
+    )
   }
 
-  tmp <- furrr::future_map_dfr(1:dim(snow_ice_thickness)[2], function(i){
-    tibble::tibble(predicted = apply(snow_ice_thickness[2:3, i, ], 2, sum),
-                   time = full_time[i],
-                   variable = "ice_thickness",
-                   depth = NA,
-                   forecast = forecast_flag[i],
-                   ensemble = 1:dim(snow_ice_thickness)[3],
-                   variable_type = "state",
-                   log_weight = log_particle_weights[i, ])
-  })
+  # --- ice thickness: snow_ice_thickness[layers, time, ens] ---
+  n_time_ice <- dim(snow_ice_thickness)[2]
+  n_ens_ice  <- dim(snow_ice_thickness)[3]
+  ice_mat <- snow_ice_thickness[2, , ] + snow_ice_thickness[3, , ]  # [time, ens]
 
-  output_list <- dplyr::bind_rows(output_list, tmp)
+  pieces$ice <- tibble::tibble(
+    predicted     = c(t(ice_mat)),
+    time          = rep(full_time[seq_len(n_time_ice)],      each = n_ens_ice),
+    variable      = "ice_thickness",
+    depth         = NA,
+    forecast      = rep(forecast_flag[seq_len(n_time_ice)],  each = n_ens_ice),
+    ensemble      = rep(seq_len(n_ens_ice), n_time_ice),
+    variable_type = "state",
+    log_weight    = c(t(log_particle_weights[seq_len(n_time_ice), ]))
+  )
+
+  output_list <- dplyr::bind_rows(pieces)
 
   time_of_forecast <- lubridate::with_tz(da_forecast_output$time_of_forecast, tzone = "UTC")
 
@@ -253,13 +269,12 @@ write_forecast <- function(da_forecast_output,
     dplyr::select(reference_datetime, datetime, pub_datetime, model_id, site_id, depth, family, parameter, variable, prediction, forecast, variable_type, log_weight)
 
   #Convert to target variable name
-  for(i in 1:length(states_config$state_names)){
-    if(length(which(obs_config$state_names_obs == states_config$state_names[i])) >0){
-      obs_name <- obs_config$target_variable[which(obs_config$state_names_obs == states_config$state_names[i])]
-      output_list <- output_list |>
-        dplyr::mutate(variable = ifelse(variable == states_config$state_names[i], obs_name, variable))
-    }
-  }
+  matched <- match(states_config$state_names, obs_config$state_names_obs)
+  has_match <- !is.na(matched)
+  name_map <- setNames(obs_config$target_variable[matched[has_match]],
+                       states_config$state_names[has_match])
+  output_list <- output_list |>
+    dplyr::mutate(variable = dplyr::coalesce(unname(name_map[variable]), variable))
 
   output_list <- output_list |>
     mutate(reference_date = lubridate::as_date(reference_datetime))
